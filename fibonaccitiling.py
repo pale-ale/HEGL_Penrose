d2e439eb129f5d1f7ded7c87127dcea192bdaeb0
pale-ale <alex.baucke@tutanota.com>
2023-04-12 10:33:22 +0200

    add projection to fibonacci tiling

--- fibonaccitiling.py
+++ fibonaccitiling.py
@@ -3,12 +3,12 @@ import math
 import numpy as np
 import sdl2
 import sdl2.ext
-
 from plotfunc import plot_func
 from squaregrid import Squaregrid
 from util import find_closest_half_point, merge_predicate
 from windowmanager import WindowManager
 from projection import project_point_line_2d
+from lineprojectionview import LineProjection
 
 WINDOWSIZE = (800, 800)
 WINDOWCENTER = (WINDOWSIZE[0]/2, WINDOWSIZE[1]/2)
@@ -21,35 +21,38 @@ wm = WindowManager("Fibonacchitiling", WINDOWSIZE)
 grid = Squaregrid(WINDOWSIZE, *GRID_SUBDIVISIONS)
 grid.origin = WINDOWCENTER
 grid.xyscale = np.array((50, -50))
+projection = LineProjection((WINDOWSIZE[0], 100), (0, WINDOWSIZE[1] - 100))
+projection.xyscale = np.array((50, 1))
+projection.origin = np.array((400, 50))
 
 PHI = (1+math.sqrt(5)) / 2
 incline = -1/PHI
-inclinescale = 1.0
+inclinescale = 0.0
+offset = 0.0
+offsetscale = 0.0
 
 
-def get_f(incline:float):
-    return lambda x: incline*x
+def get_f(incline:float, offset:float):
+    return lambda x: incline*x+offset
 
 
-def get_inv_f(incline:float):
-    return lambda x: 1/incline*x
-
-
-def is_integral(a:float):
-    return a - int(a) == 0
+def get_inv_f(incline:float, offset:float):
+    return lambda x: (x-offset)/incline
 
 
 def get_lattice_pts(startxy, endxy, func, invfunc):
     startx, starty = startxy
     endx, endy = endxy
+    ydirection = int(math.copysign(1, func(endx) - func(startx)))
     xvals = np.fromiter(range(startx, endx+1), int)
-    yvals = np.fromiter(range(min(starty, endy), max(starty, endy)+1), int)
+    yvals = np.fromiter(range(starty, endy+ydirection, ydirection), int)
     x_pts = np.array([xvals, np.apply_along_axis(func, 0, xvals)]).transpose()
     y_pts = np.array([np.apply_along_axis(invfunc, 0, yvals), yvals]).transpose()
     pts = merge_predicate(lambda p1,p2: p1[0] < p2[0], x_pts, y_pts, 0)
     lpts = np.zeros((len(pts)-1,2))
     for i in range(len(pts)-1):
         halfway = (pts[i+1] + pts[i])/2
+        grid.draw_dot_transformed(halfway, 3)
         lpts[i] = find_closest_half_point(halfway)
     return lpts
 
@@ -61,30 +64,70 @@ def draw_pt_proj_between(target:Squaregrid, p:np.ndarray, a:np.ndarray, b:np.nda
 
 
 def tickmethod():
-    global incline
+    global incline, offset
     incline += inclinescale * 0.005
+    offset += offsetscale * 0.05
     sdl2.SDL_SetRenderDrawColor(grid.renderer,0,0,0,0)
     sdl2.SDL_RenderClear(grid.renderer)
-    f, inv_f = get_f(incline), get_inv_f(incline)
-    xmin, xmax = -4, 4
+    f, inv_f = get_f(incline, offset), get_inv_f(incline, offset)
+    xmin, xmax = -10, 10
     lpts = get_lattice_pts((xmin,int(f(xmin))), (xmax,int(f(xmax))), f, inv_f)
     for lpt in lpts:
         grid.draw_dot_transformed(lpt, 6)
+    minxy,maxxy = np.array([xmin,f(xmin)]), np.array([xmax, f(xmax)])
+    proj_lattice_pts = [project_point_line_2d(p, minxy, maxxy-minxy) for p in lpts]
     plot_func(grid, np.linspace(xmin,xmax,2), f)
-    for lpt in lpts:
-        draw_pt_proj_between(grid, lpt, np.array([xmin,f(xmin)]), np.array([xmax, f(xmax)]))
+    for i in range(len(lpts)):
+        grid.draw_line_transformed(lpts[i], proj_lattice_pts[i])
+    projection.hor_segments.clear()
+    projection.vert_segments.clear()
+    projection.projection_center[1] = offset
+    for i,lpt in enumerate(lpts[1:],1):
+        diff = lpt - lpts[i-1]
+        if diff[0] == 0:
+            projection.hor_segments.append((proj_lattice_pts[i-1], proj_lattice_pts[i]))
+        else:
+            projection.vert_segments.append((proj_lattice_pts[i-1], proj_lattice_pts[i]))
+    projection.draw_pts()
     grid.draw(wm.renderer)
+    projection.draw(wm.renderer)
     sdl2.SDL_RenderPresent(grid.renderer)
 
 
-def reverse(event):
+def rot_cw(event):
     global inclinescale
     if event.type == sdl2.SDL_KEYDOWN:
         inclinescale = -1.0
         return
-    inclinescale = 1.0
+    inclinescale = 0.0
 
+
+def rot_ccw(event):
+    global inclinescale
+    if event.type == sdl2.SDL_KEYDOWN:
+        inclinescale = 1.0
+        return
+    inclinescale = 0.0
+
+
+def move_up(event):
+    global offsetscale
+    if event.type == sdl2.SDL_KEYDOWN:
+        offsetscale = 1.0
+        return
+    offsetscale = 0.0
+
+
+def move_down(event):
+    global offsetscale
+    if event.type == sdl2.SDL_KEYDOWN:
+        offsetscale = -1.0
+        return
+    offsetscale = 0.0
 
 wm.tickmethod = tickmethod
-wm.set_key_event(sdl2.keycode.SDLK_LALT, reverse)
+wm.set_key_event(sdl2.keycode.SDLK_LEFT, rot_ccw)
+wm.set_key_event(sdl2.keycode.SDLK_RIGHT, rot_cw)
+wm.set_key_event(sdl2.keycode.SDLK_DOWN, move_down)
+wm.set_key_event(sdl2.keycode.SDLK_UP, move_up)
 wm.run()
--- sprite.py
+++ sprite.py
@@ -1,17 +1,34 @@
 import sdl2.ext
 import sdl2
 import abc
+import numpy as np
+from util import iarray
+import sdl2.sdlgfx as gfx
 
 
 class BaseSprite(abc.ABC):
-    def __init__(self, size:tuple[int,int]):
+    def __init__(self, size:tuple[int,int], position:tuple[int,int]=(0,0)):
         self.surface = sdl2.SDL_CreateRGBSurface(0, *size, 32,
                                    0xff000000,  # r mask
                                    0x00ff0000,  # g mask
                                    0x0000ff00,  # b mask
                                    0x000000ff)  # a mask
         self.renderer = sdl2.render.SDL_CreateSoftwareRenderer(self.surface)
+        self.position = position
+        self.xyscale = np.array([1,1])
+        self.origin = np.array([0,0])
 
     @abc.abstractmethod
     def draw(self, target:sdl2.ext.Renderer):
         pass
+
+    def transform_point_pixel(self, p):
+        return iarray(self.xyscale * p + self.origin)
+
+    def draw_line_transformed(self, start, end, color=(255,255,255,255)):
+        gfx.lineRGBA(self.renderer, *self.transform_point_pixel(start), *self.transform_point_pixel(end), *color)
+
+    def draw_dot_transformed(self, pos, radius, color=(0,255,255,255)):
+        tfpos = iarray(self.xyscale * pos + self.origin)
+        sdl2.SDL_SetRenderDrawBlendMode(self.renderer, sdl2.SDL_BLENDMODE_NONE)
+        gfx.filledCircleRGBA(self.renderer, *(tfpos), radius, *color)
